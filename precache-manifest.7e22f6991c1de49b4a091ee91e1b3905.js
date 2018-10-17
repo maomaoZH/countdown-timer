734b17c4f63d049fe7cfa055b8e59487e1358cce
MaomaoZH <MaomaoZH@MacBook-Air-2.fritz.box>
2018-10-17 11:54:48 +0200

    Updates

---
+++ precache-manifest.7e22f6991c1de49b4a091ee91e1b3905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "149e0cbf04c96f2aa65c",
+    "url": "/countdown-timer/static/js/runtime~main.149e0cbf.js"
+  },
+  {
+    "revision": "23baf7c06ad5100055d0",
+    "url": "/countdown-timer/static/js/main.23baf7c0.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/countdown-timer/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "23baf7c06ad5100055d0",
+    "url": "/countdown-timer/static/css/main.5ea4be92.chunk.css"
+  },
+  {
+    "revision": "3229e6978689a964ecbbbf7896acd7e5",
+    "url": "/countdown-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab2f9c7f0f807075f0c1dc8e4f4511db.js"
+  "/countdown-timer/precache-manifest.7e22f6991c1de49b4a091ee91e1b3905.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/countdown-timer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

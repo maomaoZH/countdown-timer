ce861a4d04c77d2d6faef4e49fd28e18995ad94e
MaomaoZH <MaomaoZH@MacBook-Air-2.fritz.box>
2018-10-17 11:41:55 +0200

    Updates

---
+++ precache-manifest.ab2f9c7f0f807075f0c1dc8e4f4511db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fd9e45d8374e1b78e945",
+    "url": "/static/js/main.fd9e45d8.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "fd9e45d8374e1b78e945",
+    "url": "/static/css/main.5ea4be92.chunk.css"
+  },
+  {
+    "revision": "4235c205a47fd87e4ec75b8ac0d6a640",
+    "url": "/index.html"
+  }
+];

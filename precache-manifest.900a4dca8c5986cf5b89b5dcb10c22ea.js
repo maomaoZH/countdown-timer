5a32ef61e747eb7c2a593cdb3d6f1a52cda446d3
MaomaoZH <MaomaoZH@MacBook-Air-2.fritz.box>
2018-10-24 11:39:28 +0200

    Updates

---
+++ precache-manifest.900a4dca8c5986cf5b89b5dcb10c22ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "149e0cbf04c96f2aa65c",
+    "url": "/countdown-timer/static/js/runtime~main.149e0cbf.js"
+  },
+  {
+    "revision": "eb116a54f3471ed748dd",
+    "url": "/countdown-timer/static/js/main.eb116a54.chunk.js"
+  },
+  {
+    "revision": "366b727c8d0b0bfd449b",
+    "url": "/countdown-timer/static/js/1.366b727c.chunk.js"
+  },
+  {
+    "revision": "eb116a54f3471ed748dd",
+    "url": "/countdown-timer/static/css/main.802625aa.chunk.css"
+  },
+  {
+    "revision": "4e246dc287f2abab35145921754ee6a8",
+    "url": "/countdown-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/countdown-timer/precache-manifest.7e22f6991c1de49b4a091ee91e1b3905.js"
+  "/countdown-timer/precache-manifest.900a4dca8c5986cf5b89b5dcb10c22ea.js"
 );
 
 workbox.clientsClaim();
